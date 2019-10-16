ceb21033e711f58e35dcf1a012feb02f3f965ce8
Buesi <dorena@humbot.io>
2019-10-16 22:25:34 +0300

    form validation and analytics2

--- themes/kosmos/assets/js/main.js
+++ themes/kosmos/assets/js/main.js
@@ -7,17 +7,15 @@ document.addEventListener('click', function (event) {
 	if (event.target.matches('#closeMenu')) {
 		document.getElementById('menuContainer').style.display = "none";
 	}
-
-	$("#applicationForm").submit(function(e) {
-		e.preventDefault();	
-
-	    var $form = $(this);
-	    $.post($form.attr("action"), $form.serialize()).then(function() {
-	     	document.getElementById('hideLater').style.display = "none";
-			document.getElementById('confirmation').style.display = "block";
-	    });
-	});
-
 }, false);
 
 
+$("#applicationForm").submit(function(e) {
+	e.preventDefault();	
+
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+     	document.getElementById('hideLater').style.display = "none";
+		document.getElementById('confirmation').style.display = "block";
+    });
+});
